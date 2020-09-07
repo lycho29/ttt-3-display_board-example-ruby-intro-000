f77d117e7602731b2dad145089f058c094238d10
Lydia Cho <lydiacho@gse.havard.edu>
2020-09-07 13:59:45 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
-display_board
+def display_board
+  puts "A Tic Tac Toe Board"
+end
+
 puts "   |   |   "
 puts "-----------"
 puts "   |   |   "
